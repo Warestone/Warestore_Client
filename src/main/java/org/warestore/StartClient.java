05d8721a36af14461a208ebf476c198559f9fdf3
Eugene Timofeev <jontimofeev@yandex.ru>
2021-02-19 18:25:31 +0300

    commit project

---
+++ src/main/java/org/warestore/StartClient.java
@@ -0,0 +1,11 @@
+package org.warestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartClient {
+    public static void main(String[] args) {
+        SpringApplication.run(StartClient.class, args);
+    }
+}
