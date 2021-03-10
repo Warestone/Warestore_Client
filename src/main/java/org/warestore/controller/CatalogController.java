fb94deea420fe45a66851673840c9a2b67a76111
Eugene Timofeev <jontimofeev@yandex.ru>
2021-03-10 16:37:34 +0300

    some fixes with URL's & pages

--- src/main/java/org/warestore/controller/CatalogController.java
+++ src/main/java/org/warestore/controller/CatalogController.java
@@ -1,6 +1,7 @@
 package org.warestore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,20 +16,23 @@ import java.util.List;
 public class CatalogController {
 
     @Autowired
+    Environment environment;
+
+    @Autowired
     private RequestService requestService;
 
     @GetMapping("/")
     public String getIndexPage(Model model){
         List<Category> categories = (List<Category>) requestService.
-                getData("http://localhost:2033/server/catalog/get/category");
+                getOrPostData(environment.getProperty("url.categories"));
         model.addAttribute("categoriesList", categories);
         return "index";
     }
 
     @GetMapping("/rifles")
     public String getRiflesPage(Model model, String page){
-        List<Weapon> rifles = (List<Weapon>) requestService.getData(requestService.
-                getURL("http://localhost:2033/server/catalog/get/rifle_page/", page)
+        List<Weapon> rifles = (List<Weapon>) requestService.getOrPostData(requestService.
+                getURL(environment.getProperty("url.rifles"), page)
         );
         model.addAttribute("riflesList", rifles);
         return "rifles";
@@ -39,8 +43,8 @@ public class CatalogController {
 
     @GetMapping("/shotguns")
     public String getShotgunsPage(Model model, String page){
-        List<Weapon> shotguns = (List<Weapon>) requestService.getData(requestService.
-                getURL("http://localhost:2033/server/catalog/get/shotgun_page/", page)
+        List<Weapon> shotguns = (List<Weapon>) requestService.getOrPostData(requestService.
+                getURL(environment.getProperty("url.shotguns"), page)
         );
         model.addAttribute("shotgunsList", shotguns);
         return "shotguns";
@@ -50,8 +54,8 @@ public class CatalogController {
 
     @GetMapping("/airguns")
     public String getAirgunsPage(Model model, String page){ ;
-        List<Weapon> airguns = (List<Weapon>) requestService.getData(requestService.
-                        getURL("http://localhost:2033/server/catalog/get/airgun_page/", page));
+        List<Weapon> airguns = (List<Weapon>) requestService.getOrPostData(requestService.
+                        getURL(environment.getProperty("url.airguns"), page));
         model.addAttribute("airgunsList", airguns);
         return "airguns";
     }
@@ -60,8 +64,8 @@ public class CatalogController {
 
     @GetMapping("/ammo")
     public String getAmmoPage(Model model, String page){
-        List<Ammo> ammo = (List<Ammo>) requestService.getData(requestService.
-                getURL("http://localhost:2033/server/catalog/get/ammo_page/", page));
+        List<Ammo> ammo = (List<Ammo>) requestService.getOrPostData(requestService.
+                getURL(environment.getProperty("url.ammo"), page));
         model.addAttribute("ammoList", ammo);
         return "ammo";
     }
@@ -70,8 +74,8 @@ public class CatalogController {
 
     @GetMapping("/targets")
     public String getTargetPage(Model model, String page){
-        List<Target> targets = (List<Target>) requestService.getData(requestService.
-                getURL("http://localhost:2033/server/catalog/get/target_page/", page));
+        List<Target> targets = (List<Target>) requestService.getOrPostData(requestService.
+                getURL(environment.getProperty("url.targets"), page));
         model.addAttribute("targetList", targets);
         return "targets";
     }
--- src/main/java/org/warestore/service/RequestService.java
+++ src/main/java/org/warestore/service/RequestService.java
@@ -1,29 +1,33 @@
 package org.warestore.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import lombok.extern.java.Log;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
 
+@Log
 @Service
 public class RequestService {
 
     private final Environment environment;
     private final RestTemplate restTemplate = new RestTemplate();
+    private final PasswordEncoder passwordEncoder;
 
-    public RequestService(Environment environment) {
+    public RequestService(Environment environment, PasswordEncoder passwordEncoder) {
         this.environment = environment;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public List<?>getData(String url){
+    public List<?> getOrPostData(String url){
+        log.info("Return data for '"+url+"'");
         HttpEntity<String> entity = new HttpEntity<>("parameters", getHeaders());
         ResponseEntity<List<?>> response = restTemplate.exchange(
                 url,
@@ -41,7 +45,8 @@ public class RequestService {
     private HttpHeaders getHeaders(){
         //get token
         HttpHeaders headers = new HttpHeaders();
-        headers.add("App-Verification",environment.getProperty("app.secret"));
+        headers.add("App-Verification",
+                passwordEncoder.encode(environment.getProperty("app.secret")));
         //headers.add("Authorization","Bearer "+token);
         return headers;
     }
